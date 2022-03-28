1cef944a5d661dda622374f92055abcfd26a0700
DavidPsyko <davidjohnsonny@gmail.com>
2022-03-28 15:22:06 -0700

    part 3 search bar is done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import "mdbreact/dist/css/mdb.css";
 function App() {
 
   const [student, setStudent] = useState([]);
-  const [avgScore, setAvgScore] = useState([]);
   const [entry, setEntry] = useState("");
   const [filteredResults, setFilteredResults] = useState([]);
 
@@ -26,44 +25,29 @@ function App() {
           console.log(err.message);
         })
       }, []);
-      
-      
-      // let grad = student
-      // const finalScore = [];
-      // for (let i = 0; i < grad.length; i++) {
-      //   let listGrades = grad[i].grades;
-      //   let numberArray = listGrades.map(Number);
-      //   const sum = numberArray.reduce((a, b) => a + b, 0);
-      //   const finalAvg = (sum / numberArray.length) || 0;
-      //   finalScore.push(finalAvg);
-      //   setAvgScore(finalScore);
-  // useEffect(() => {
-  //   let grad = student
-  //   function Average(grad) {
-
-  //     }
-  //   }
-  // }, []);
-    
-  
-    const filterName = (searchName) => {
-    setEntry(searchName);
-    setFilteredResults(student)
-    const filteredData = student.filter((item) => {
-      let tempfullname = item.firstName.concat(" ", item.lastName);
-      return tempfullname.toLowerCase().includes(entry.toLowerCase())
-    })
-    setFilteredResults(filteredData)
-    console.log(filteredResults)
+         
+  const calScore = (listofgrades) => {
+    let numbArray = listofgrades.map(Number)
+    const sum = numbArray.reduce((a, b) => a + b, 0);
+    const finalAvg = (sum / numbArray.length) || 0;
+    return finalAvg;
   }
 
-
+  useEffect(() => {
+    console.log(entry)
+      const filteredData = student.filter((item) => {
+        let tempfullname = item.firstName.concat(" ", item.lastName);
+        return tempfullname.toLowerCase().includes(entry.toLowerCase())
+      })
+      setFilteredResults(filteredData)
+      console.log(filteredResults)
+    }, [entry])
 
   return (
     <div id="body">
       <div class="container w-50 p-3 border-bottom">
         <MDBCol md="12">
-          <MDBInput hint="Search" type="text" containerClass="mt-0" onChange={(event) => { filterName(event.target.value) }} />
+          <MDBInput hint="Search" type="text" containerClass="mt-0" onChange={(event) => {setEntry(event.target.value)}} />
         </MDBCol>
       </div>
       <div>
@@ -77,7 +61,7 @@ function App() {
                   <div>Email: {dude.email}</div>
                   <div>Company: {dude.company}</div>
                   <div>Skill: {dude.skill}</div>
-                  <div>Average: {avgScore}%</div>
+                  <div>Average: {calScore(dude.grades)}%</div>
                 </div>
               </div>
             </div>
--- src/scratchpaper.js
+++ src/scratchpaper.js
@@ -1,91 +1,76 @@
-// import React, { useEffect, useState } from "react";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
-// import { MDBInput, MDBCol } from "mdbreact";
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import "bootstrap-css-only/css/bootstrap.min.css";
-// import "mdbreact/dist/css/mdb.css";
+import React, { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { MDBInput, MDBCol } from "mdbreact";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
-// function App() {
+function App() {
 
-//     const [student, setStudent] = useState([]);
-//     const [avgScore, setAvgScore] = useState([]);
-//     const [entry, setEntry] = useState("");
-//     const [filteredResults, setFilteredResults] = useState([]);
+    const [student, setStudent] = useState([]);
+    const [entry, setEntry] = useState("");
+    const [filteredResults, setFilteredResults] = useState([]);
 
-//     useEffect(() => {
-//         fetch("https://api.hatchways.io/assessment/students")
-//             .then(response => {
-//                 return response.json();
-//             })
-//             .then(data => {
-//                 setStudent(data.students);
-//                 setFilteredResults(data.students)
-//             })
-//             .catch((err) => {
-//                 console.log(err.message);
-//             })
-//     }, []);
+    useEffect(() => {
+        fetch("https://api.hatchways.io/assessment/students")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setStudent(data.students);
+                setFilteredResults(data.students)
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+    }, []);
 
-//     let grad = student
-//     useEffect(() => {
-//         let average = function Average(grad) {
-//             const finalScore = [];
-//             for (let i = 0; i < grad.length; i++) {
-//                 let listGrades = grad[i].grades;
-//                 let numberArray = listGrades.map(Number);
-//                 const sum = numberArray.reduce((a, b) => a + b, 0);
-//                 const finalAvg = (sum / numberArray.length) || 0;
-//                 finalScore.push(finalAvg);
-//                 setAvgScore(finalScore);
+    const calScore = (listofgrades) => {
+        let numbArray = listofgrades.map(Number)
+        const sum = numbArray.reduce((a, b) => a + b, 0);
+        const finalAvg = (sum / numbArray.length) || 0;
+        return finalAvg;
+    }
 
-//             }
-//         }
-//     })
+    useEffect(() => {
+        console.log(entry)
+        const filteredData = student.filter((item) => {
+            let tempfullname = item.firstName.concat(" ", item.lastName);
+            return tempfullname.toLowerCase().includes(entry.toLowerCase())
+        })
+        setFilteredResults(filteredData)
+        console.log(filteredResults)
+    }, [entry])
 
-//     Average(grad);
+    return (
+        <div id="body">
+            <div class="container w-50 p-3 border-bottom">
+                <MDBCol md="12">
+                    <MDBInput hint="Search" type="text" containerClass="mt-0" onChange={(event) => { setEntry(event.target.value) }} />
+                </MDBCol>
+            </div>
+            <div>
+                {filteredResults.map((dude) => (
+                    <div class='container w-50 p-3 border-bottom' key={dude.id}>
+                        <div class="row pl-5">
+                            <img class="" alt="" id="image" src={dude.pic}></img>
+                            <div class="col">
+                                <h1>{dude.firstName + " " + dude.lastName}</h1>
+                                <div class="container">
+                                    <div>Email: {dude.email}</div>
+                                    <div>Company: {dude.company}</div>
+                                    <div>Skill: {dude.skill}</div>
+                                    <div>Average: {calScore(dude.grades)}%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
 
-//     const filterName = (searchName) => {
-//         if (searchName)
-//             setEntry(searchName);
-//         setFilteredResults(student)
-//         const filteredData = student.filter((item) => {
-//             let tempfullname = item.firstName.concat(" ", item.lastName);
-//             return tempfullname.toLowerCase().includes(entry.toLowerCase())
-//         })
-//         setFilteredResults(filteredData)
-//     }
-
-
-
-//     return (
-//         <div id="body">
-//             <div class="container w-50 p-3 border-bottom">
-//                 <MDBCol md="12">
-//                     <MDBInput hint="Search" type="text" containerClass="mt-0" onChange={(event) => { filterName(event.target.value) }} />
-//                 </MDBCol>
-//             </div>
-//             <div>
-//                 {filteredResults.map((dude) => (
-//                     <div class='container w-50 p-3 border-bottom' key={dude.id}>
-//                         <div class="row pl-5">
-//                             <img class="" alt="" id="image" src={dude.pic}></img>
-//                             <div class="col">
-//                                 <h1>{dude.firstName + " " + dude.lastName}</h1>
-//                                 <div class="container">
-//                                     <div>Email: {dude.email}</div>
-//                                     <div>Company: {dude.company}</div>
-//                                     <div>Skill: {dude.skill}</div>
-//                                     <div>Average: {avgScore}%</div>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default App;
+export default App;
 
